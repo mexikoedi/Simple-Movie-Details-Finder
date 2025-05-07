c68e5d04cfafc8140518645132b33fd91bd97fe1
mexikoedi <mexikoedi@outlook.com>
2025-05-07 18:44:45 +0200

    fix java version issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,12 +43,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
 
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "20"
     }
 
     buildFeatures {
